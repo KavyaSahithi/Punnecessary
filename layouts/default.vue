24efdf596bbba0041a5a5a3f4f3c257b34346e88
Karthik Nayak <karthik.188@gmail.com>
2020-06-28 10:03:02 +0530

    Finished homepage
    Signed-off-by: Karthik Nayak <karthik.188@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,46 @@
 <template>
   <div>
-    <nuxt/>
+    <b-navbar fixed-top type="is-primary">
+      <template slot="brand">
+        <b-navbar-item>
+          <nuxt-link class="has-text-white" to="/portfolio">Punnecessary</nuxt-link>
+        </b-navbar-item>
+      </template>
+      <template slot="end">
+        <b-navbar-item>
+          <nuxt-link class="has-text-white" to="/portfolio">Portfolio</nuxt-link>
+        </b-navbar-item>
+        <b-navbar-item>
+          <nuxt-link class="has-text-white" to="/contact">Contact</nuxt-link>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+    <nuxt />
   </div>
 </template>
 
+<style lang="scss">
+@import 'assets/scss/main.scss';
+
+.navbar-brand .has-text-white {
+  font-weight: bold;
+  padding-left: 4vw;
+}
+
+.navbar-end {
+  padding-right: 4vw;
+}
+
+.navbar-burger span {
+  background-color: $white;
+}
+
+.navbar-menu {
+  background-color: $primary;
+}
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <section class="section">
-    Hello
+  <section>
+    <figure class="image container">
+      <img src="~/assets/logo.PNG"/>
+    </figure>
   </section>
 </template>
+
+<style lang="scss" scoped>
+@import 'assets/scss/main.scss';
+
+.image {
+  padding-top: calc(15vh);
+  width: calc(350px + 8vw);
+}
+
+section {
+  min-height: 100vh;
+  background-color: $primary;
+}
+</style>
