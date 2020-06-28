ef97f1422bb7d64ac9216ebc04a54b02649adb53
Karthik Nayak <karthik.188@gmail.com>
2020-06-28 22:03:01 +0530

    Finished Contacts
    Signed-off-by: Karthik Nayak <karthik.188@gmail.com>

--- components/ComicsComponent.vue
+++ components/ComicsComponent.vue
@@ -1,21 +1,117 @@
 <template>
-  <div class="tile is-ancestor">
-    <div class="tile is-4 is-vertical is-parent">
-      <div class="tile is-child box">
-        <p class="title">One</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+  <div>
+    <div class="tile is-ancestor">
+      <div class="tile is-3 is-vertical is-parent">
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('sleep')"></cloudinary-component>
+        </div>
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('pizza')"></cloudinary-component>
+        </div>
       </div>
-      <div class="tile is-child box">
-        <p class="title">Two</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+      <div class="tile is-3 is-vertical is-parent">
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('marriage')"></cloudinary-component>
+        </div>
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('towel')"></cloudinary-component>
+        </div>
+      </div>
+      <div class="tile is-3 is-vertical is-parent">
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('angry')"></cloudinary-component>
+        </div>
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('ac')"></cloudinary-component>
+        </div>
+      </div>
+      <div class="tile is-3 is-vertical is-parent">
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('magic')"></cloudinary-component>
+        </div>
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('slice')"></cloudinary-component>
+        </div>
       </div>
     </div>
-    <div class="tile is-parent">
-      <div class="tile is-child box">
-        <p class="title">Three</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend, nunc dui porta orci, quis semper odio felis ut quam.</p>
-        <p>Suspendisse varius ligula in molestie lacinia. Maecenas varius eget ligula a sagittis. Pellentesque interdum, nisl nec interdum maximus, augue diam porttitor lorem, et sollicitudin felis neque sit amet erat. Maecenas imperdiet felis nisi, fringilla luctus felis hendrerit sit amet. Aenean vitae gravida diam, finibus dignissim turpis. Sed eget varius ligula, at volutpat tortor.</p>
-        <p>Integer sollicitudin, tortor a mattis commodo, velit urna rhoncus erat, vitae congue lectus dolor consequat libero. Donec leo ligula, maximus et pellentesque sed, gravida a metus. Cras ullamcorper a nunc ac porta. Aliquam ut aliquet lacus, quis faucibus libero. Quisque non semper leo.</p>
+
+    <div class="tile is-ancestor">
+      <div class="tile is-4 is-vertical is-parent">
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('salad')"></cloudinary-component>
+        </div>
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('rat')"></cloudinary-component>
+        </div>
+      </div>
+      <div class="tile is-4 is-vertical is-parent">
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('moon')"></cloudinary-component>
+        </div>
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('momeat')"></cloudinary-component>
+        </div>
+      </div>
+      <div class="tile is-4 is-vertical is-parent">
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('deserve')"></cloudinary-component>
+        </div>
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('netflix')"></cloudinary-component>
+        </div>
+      </div>
+    </div>
+
+    <div class="tile is-ancestor">
+      <div class="tile is-3 is-vertical is-parent">
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('candy')"></cloudinary-component>
+        </div>
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('comb')"></cloudinary-component>
+        </div>
+      </div>
+      <div class="tile is-3 is-vertical is-parent">
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('donut')"></cloudinary-component>
+        </div>
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('tshirt')"></cloudinary-component>
+        </div>
+      </div>
+      <div class="tile is-3 is-vertical is-parent">
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('bubblewrap')"></cloudinary-component>
+        </div>
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('birthday')"></cloudinary-component>
+        </div>
+      </div>
+      <div class="tile is-3 is-vertical is-parent">
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('cheerleader')"></cloudinary-component>
+        </div>
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('sorrow')"></cloudinary-component>
+        </div>
+      </div>
+    </div>
+
+    <div class="tile is-ancestor">
+      <div class="tile is-4 is-vertical is-parent">
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('ready')"></cloudinary-component>
+        </div>
+      </div>
+      <div class="tile is-4 is-vertical is-parent">
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('soap')"></cloudinary-component>
+        </div>
+      </div>
+      <div class="tile is-4 is-vertical is-parent">
+        <div class="tile is-child">
+          <cloudinary-component category="Comics" :id="images.get('icecream')"></cloudinary-component>
+        </div>
       </div>
     </div>
   </div>
@@ -26,8 +122,39 @@ import Vue from 'vue'
 import CloudinaryComponent from '@/components/CloudinaryComponent.vue'
 
 export default Vue.extend({
-    components: {
-        CloudinaryComponent
+  components: {
+    CloudinaryComponent,
+  },
+  data() {
+    return {
+      images: new Map([
+        ['pizza', '1_csnlia'],
+        ['marriage', '2__vjgjyf'],
+        ['towel', '3__zygxgy'],
+        ['angry', '4_qe13z8'],
+        ['ac', '5_uhnyte'],
+        ['magic', '6_wv3m2n'],
+        ['sleep', '7_k3kieo'],
+        ['slice', '8_hubymt'],
+        ['salad', '9_mmojs2'],
+        ['deserve', '10_j1ze49'],
+        ['moon', '11_l3jukn'],
+        ['rat', 'IMG_0360_mwyrlx'],
+        ['momeat', 'IMG_0411_dvbdrs'],
+        ['netflix', 'IMG_0653_tm47zr'],
+        ['candy', 'IMG_0800_yovtsl'],
+        ['donut', 'IMG_0802_xmatby'],
+        ['bubblewrap', 'IMG_0807_psvnnv'],
+        ['cheerleader', 'IMG_1292_wbjjnw'],
+        ['comb', 'IMG_1305_sgohss'],
+        ['tshirt', 'IMG_1307_shxg4u'],
+        ['birthday', 'IMG_1347_myjnzl'],
+        ['sorrow', 'IMG_1443_vtkga4'],
+        ['ready', 'IMG_1571_em5dqt'],
+        ['soap', 'IMG_1793_ovkpz5'],
+        ['icecream', 'IMG_1623_obkscq'],
+      ]),
     }
+  },
 })
 </script>
---
+++ pages/contact.vue
@@ -0,0 +1,64 @@
+<template>
+  <section>
+    <figure class="container">
+      <div class="top-level-fields">
+        <div class="field has-text-white">
+          <span>
+            <b-icon icon="instagram" size="is-large" type="is-white"></b-icon>
+          </span>
+          <span>@_punnecessary</span>
+        </div>
+        <div class="field has-text-white">
+          <span>
+            <b-icon icon="facebook" size="is-large" type="is-white"></b-icon>
+          </span>
+          <span>fb.com/punnecessary</span>
+        </div>
+        <div class="field has-text-white">
+          <span>
+            <b-icon icon="google-chrome" size="is-large" type="is-white"></b-icon>
+          </span>
+          <span>www.kavyasahithi.com</span>
+        </div>
+        <div class="field has-text-white">
+          <span>
+            <b-icon icon="whatsapp" size="is-large" type="is-white"></b-icon>
+          </span>
+          <span>9620388230 / 9741104864</span>
+        </div>
+        <div class="field has-text-white">
+          <span>
+            <b-icon icon="email" size="is-large" type="is-white"></b-icon>
+          </span>
+          <span>kavya.lily@gmail.com</span>
+        </div>
+      </div>
+    </figure>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import 'assets/scss/main.scss';
+@import url('https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap');
+
+.field {
+  font-family: 'Gochi Hand', cursive;
+  font-size: 3vw;
+}
+
+.container {
+  padding-top: calc(15vh);
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.top-level-fields {
+  flex-direction: columns;
+}
+
+section {
+  min-height: 100vh;
+  background-color: $primary;
+}
+</style>
